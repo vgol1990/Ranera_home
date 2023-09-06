f20301d651eb71f4faf60c195dbd563254e41447
vgol1206 <smexova@bk.ru>
2023-09-06 10:19:06 +0300

    complete3task
    сделал 3 задачу

--- lvl_3/task_3.1.py
+++ lvl_3/task_3.1.py
@@ -26,3 +26,46 @@
 #   - использовать готовые классы numpy.array() и pandas.DataFrame() запрещено!
 #   - проявите фантазию :)
 
+class Matrica():
+
+    def __init__(self) -> None:
+        self.a = 0
+        self.b = 0
+        return
+
+    def Input_Matrica(self):
+        self.a = int(input('Число строк='))
+        self.b = int(input('Число колонок='))
+        self.arr = [[[] for i in range(self.b)] for j in range(self.a)]
+
+        for i in range(self.a):
+            for j in range(self.b):
+                print('X(', i, ',', j, ')=', end='')
+                self.arr[i][j] = input('?')
+        return
+
+    def Print_Matrica(self, Text):
+        print(Text)
+        for i in range(self.a):
+            print(self.arr[i])
+        return
+
+    def Change_Matrica(self):
+        i = int(input('Введите номер строки:'))
+        j = int(input('Введите номер колонки:'))
+        NewXij = input('Введите новое значение=')
+        self.arr[i - 1][j - 1] = NewXij
+        return
+
+    def RowsColumns(self):
+        print('Матрица содержит:')
+        print('Строк:', self.a, '\nКолонок:', self.b)
+        return
+
+
+c = Matrica()
+c.Input_Matrica()
+c.Print_Matrica('Новая матрица:')
+c.Change_Matrica()
+c.Print_Matrica('Изменённая матрица:')
+c.RowsColumns()
--- lvl_4/task_4.1.py
+++ lvl_4/task_4.1.py
@@ -20,3 +20,82 @@
 # Имя студента:
 # ID школы:
 # Название школы:
+
+import sqlite3
+
+
+def CreateTable():
+    con = sqlite3.connect('teatchers.db')
+    cur = con.cursor()
+    sql = """
+        CREATE TABLE IF NOT EXISTS Students(
+            Student_id INTEGER NOT NULL,
+            Student_Name TEXT NOT NULL,
+            School_id INTEGER NOT NULL PRIMARY KEY
+
+        )"""
+
+    sql = """
+          CREATE TABLE IF NOT EXISTS School(
+          School_id INTEGER NOT NULL,
+          School_name TEXT NOT NULL
+        )
+    """
+    cur.execute(sql)
+    return
+
+
+def close_connection(connection):
+    if connection:
+        connection.close()
+
+
+def Insert_table():
+    con = sqlite3.connect('teatchers.db')
+    cur = con.cursor()
+
+    sql = '''INSERT INTO Students
+        VALUES 
+            (? ,? ,?)'''
+    arr = [
+        (201, 'Иван', 1),
+        (202, 'Петр', 2),
+        (203, 'Анастасия', 3),
+        (204, 'Игорь', 4)
+    ]
+    #cur.executemany(sql, arr)
+
+    sql = '''INSERT INTO School
+            VALUES  (?,?)'''
+    arr = [
+        ('1', 'Альтернатива'),
+        ('2', 'Армат'),
+        ('3', 'Новатор'),
+        ('4', 'Эрудит')
+    ]
+    cur.executemany(sql, arr)
+
+    con.commit()
+    con.close()
+    return
+
+
+def Select_from_table():
+    id = input('Введите id студента')
+    con = sqlite3.connect('teatchers.db')
+    cur = con.cursor()
+    sql = 'SELECT * FROM Students'
+    # sql = 'SELECT Student_Id, Student_Name, School_Id FROM Students WHERE Student_Id = ?", (id)'
+    cur.execute(sql)
+    arr = cur.fetchall()
+    for i in range(len(arr)):
+        print('id студента:', arr[1][0])
+        print('Имя студента', arr[1][1])
+        print('id школы:', arr[1][2])
+        print('________________')
+
+
+
+CreateTable()
+Insert_table()
+Select_from_table()
